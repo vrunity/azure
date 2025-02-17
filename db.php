f2e2b677cc1e0b6659f27f96a2cc8b66d6dbf713
vrunity <145751145+vrunity@users.noreply.github.com>
2025-02-17 15:53:01 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Display a basic message
-echo "<h2>Welcome to My PHP App on Azure!</h2>";
+echo "<h2>Welcome to My App!</h2>";
 echo "<p>Your Azure App Service is working correctly.</p>";
 
 // Display PHP information (Useful for debugging)
